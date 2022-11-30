d92273baea49287ad5876e338c902d9d805c45d1
Noppadon Piya-isarakul <npiya-is@z3t7c1.42bangkok.com>
2022-11-30 20:01:37 +0700

    add semaphore before take fork

--- philo_bonus/srcs_bonus/routines_bonus.c
+++ philo_bonus/srcs_bonus/routines_bonus.c
@@ -6,7 +6,7 @@
 /*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 21:23:00 by npiya-is          #+#    #+#             */
-/*   Updated: 2022/11/28 21:22:25 by npiya-is         ###   ########.fr       */
+/*   Updated: 2022/11/30 19:53:33 by npiya-is         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,14 @@ void	eating(t_philo *philo, t_data data)
 	{
 		print_time(philo, "is eating\n", data);
 		take_time(philo, data.time_to_eat, data);
-		sem_wait(data.lock);
+		sem_wait(data.eatall);
 		philo->time_eat = philo->time;
 		get_time(philo, data);
 		philo->num_eat++;
 		if (philo->num_eat == data.num_must_eat)
 			data.eat++;
 		philo->time_not_eat = 0;
-		sem_post(data.lock);
+		sem_post(data.eatall);
 	}
 }
 
@@ -52,8 +52,10 @@ void	take_fork(t_philo *philo, t_data data)
 	get_time(philo, data);
 	if (!data.die && data.num_philo != data.eat)
 	{
+		sem_wait(data.lock);
 		choose_fork(philo, data.fork, data);
 		choose_fork(philo, data.fork, data);
+		sem_post(data.lock);
 		eating(philo, data);
 		drop_fork(philo, data.fork);
 		drop_fork(philo, data.fork);
